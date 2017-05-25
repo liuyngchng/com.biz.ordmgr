e931bf8329f75d2edb9e147130b8a1a100e75b13
Richard Liu <liuyongcheng@souyidai.com>
2017-05-25 14:46:39 +0800

    debug.

---
+++ src/test/java/com/biz/ordmgr/controller/HealthCheckControllerTest.java
@@ -0,0 +1,13 @@
+package com.biz.ordmgr.controller;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Richard on 5/25/17.
+ */
+public class HealthCheckControllerTest extends TestCase {
+
+    public void testHealth() throws Exception {
+
+    }
+}
